69e55d07d1340072b68e851f0c0f9588d48146a9
VaatiV <129503340+VaatiV@users.noreply.github.com>
2023-10-05 12:16:41 +0300

    Update and rename 04_initials.py to 07_initials.py

---
+++ 1-hello-world/07_initials.py
@@ -0,0 +1,9 @@
+# Fun fact: My high school band Attica was signed to an indie record label.
+
+print(' VVV   L    ')
+print('V   V  L    ')
+print('V      L    ')
+print(' V V V   L    ')
+print('    V  L    ')
+print('V   V  L    ')
+print(' VVV   LLLLL')
